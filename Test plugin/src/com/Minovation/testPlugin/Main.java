00ba17bd9c93c10f288c8a2088ca7c02227fa62a
Lux <acourtn2@illinois.edu>
2016-08-14 18:18:21 -0500

    actually added test plugin now

---
+++ Test plugin/src/com/Minovation/testPlugin/Main.java
@@ -0,0 +1,18 @@
+package com.Minovation.testPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public void onEnable(){
+		getLogger().info("Hello World????????????????????/");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("hello")){
+			sender.sendMessage("world word");
+		}
+		return false;
+	}
+	
+}
